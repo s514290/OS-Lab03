1fb4dbfdbd601f0858ec653611d0c54fedd78c3d
Chester <s514290@nwmissouri.edu>
2017-10-18 13:27:32 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/CCondray_Lab03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CCondray_Lab03.dir/main.cpp.obj"
+  "CCondray_Lab03.pdb"
+  "CCondray_Lab03.exe"
+  "CCondray_Lab03.exe.manifest"
+  "libCCondray_Lab03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCondray_Lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
